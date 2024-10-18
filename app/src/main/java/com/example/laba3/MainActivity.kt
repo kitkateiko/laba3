fb62b3b93a4d87c090ea22280fe27c732fe30b4d
kitkateiko <tyrkov2003@mail.ru>
2024-10-18 18:11:55 +0700

    interface

--- app/src/main/java/com/example/laba3/MainActivity.kt
+++ app/src/main/java/com/example/laba3/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.laba3
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.Toast
+import android.view.View
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,22 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+    fun formula(view: View)
+    {
+
+
+        if (findViewById<EditText>(R.id.editTextText).text.toString().equals(""))
+        {
+            return
+        }
+        val x = findViewById<EditText>(R.id.editTextText).text.toString().toCharArray()
+        val i = findViewById<EditText>(R.id.editTextNumberSigned).text.toString().toInt()
+        val c = findViewById<EditText>(R.id.editTextText2).text.toString().toCharArray()
+        for(n in 0..i-1)
+        {
+            x[n] = c[0]
+        }
+        Toast.makeText(this, String(x), Toast.LENGTH_SHORT).show()
     }
 }
